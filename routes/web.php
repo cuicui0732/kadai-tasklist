0fd266fdd38457d25e8d7f98a737c79f3faeedfb
“cuicui” <puchi.panda7@gmail.com>
2021-03-06 03:23:12 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,12 @@
 
 
 Route::get('/', 'TasksController@index');
+Route::resource('tasks', 'TasksController');
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('tasks', 'TasksController');
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
